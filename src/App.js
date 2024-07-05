97b94570e50c870b49d7fb2b4ca0778f357ea5cb
ifedayo <ifedayyo12@gmail.com>
2024-07-05 13:38:41 +0100

    updated the undo button

--- src/App.js
+++ src/App.js
@@ -73,6 +73,11 @@ function TabContent({ item }) {
     setLikes(likes + 3);
   }
 
+  function handleUndo() {
+    setShowDetails(true);
+    setLikes(0);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -91,7 +96,7 @@ function TabContent({ item }) {
       </div>
 
       <div className="tab-undo">
-        <button>Undo</button>
+        <button onClick={handleUndo}>Undo</button>
         <button>Undo in 2s</button>
       </div>
     </div>
